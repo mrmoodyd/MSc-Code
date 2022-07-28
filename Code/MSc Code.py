Fixed calculation of distribution mean

Code/MSc Code.py
@@ -4,6 +4,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from matplotlib.lines import Line2D
 plt.rcParams['animation.ffmpeg_path'] = r'C:\FFmpeg\bin\ffmpeg'
 from scipy.stats import norm, skewnorm
 from scipy.sparse import diags
@@ -18,32 +19,40 @@ def findMoment(n,ydata):
     """
     Takes the y data of the pdf and calculates the central moment specified by n.
     """
-    mean = np.dot(x, ydata)
+    mean = np.dot(x, ydata/np.sum(ydata))
     return np.dot((x-mean)**n,ydata)
 
-def evolutionAnimation(saveFig = False):
+def evolutionAnimation(plotData, saveFig = False):
     """
     Displays an animated plot showing the evolution of the calculated distribution over time.
     """
     fig, ax = plt.subplots()
+
     text = ax.text(0.02, 0.84, '\n'.join(('','','')), transform=ax.transAxes, bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
-    line, = ax.plot(x, Wdata_3[0], color='k')
-    #ax.set_xlim(1-1.3e-12,1+1.3e-12)
+    line, = ax.plot(x, plotData[0], color='k', label='Plotted Data')
+    #line1, = ax.plot(x, exactData[0], label = 'Exact Data')
+    meanLine = Line2D([0], [0], color='red', label='Mean')
+
+    handles, labels = plt.gca().get_legend_handles_labels()
+    handles.extend([meanLine])
+    legend = ax.legend(handles=handles)
+
     ax.set_ylim(0,0.01)
-    ax.set_title('Earth-Moon System ($D^{(1)} = 0, D^{(2)} = $Constant)')
+    ax.set_title('Earth-Moon System')
     ax.set_ylabel('Probability')
     ax.set_xlabel('Period / $\~P$')
 
-    def animate(i, Wdata_3, exactData):
+    def animate(i, plotData, exactData):
         """
         Defines the animation function used by 'FuncAnimation'.
         """
-        text.set_text('\n'.join(('Time = {:.1f} $P_0$ (Orbits)'.format(i*dt,2),'Probability Loss = {:.2e}'.format(abs(sum(Wdata_3[0]) - sum(Wdata_3[i]))),'Fourth C Moment = '+str(findMoment(4, Wdata_3[i])/findMoment(2, Wdata_3[i])**2))))#Standard Deviation = {:.2e} $P_0$'.format(np.sqrt(findMoment(2,Wdata_3[i]))))))
-        line.set_data(x, Wdata_3[i])
-        return line, text,
+        text.set_text('\n'.join(('Time = {:.1f} $P_0$ (Orbits)'.format(i*dt,2),'Probability Loss = {:.2e}'.format(abs(sum(plotData[0]) - sum(plotData[i]))),'Mean Drift = {:.2e} $P_0$'.format(np.dot(x,plotData[i]/np.sum(plotData[i]))-np.dot(x,exactData[i]/np.sum(exactData[i]))))))
+        line.set_data(x, plotData[i])
+        meanLine = ax.axvline(np.dot(x,plotData[i]/np.sum(plotData[i])), color='red', label='Mean')
+        return line, text, meanLine
 
     #Runs the animation.
-    ani = animation.FuncAnimation(fig, animate, fargs = [Wdata_3, exactData], frames=T, interval=1, blit = True, repeat=False)
+    ani = animation.FuncAnimation(fig, animate, fargs = [plotData, exactData], frames=T, interval=1, blit = True, repeat=False)
 
     #Saves animation if required.
     if saveFig:
@@ -52,15 +61,15 @@ def evolutionAnimation(saveFig = False):
     plt.show()
 
 #Defines the finite differencing size for the space (dx) and time (dt) dimensions.
-dx = 0.5e-14
-dt = 1
+dx = 1e-14
+dt = 10
 
 #Defines the x-axis to be plotted against.
-x = np.arange(1-5e-12,1+5e-12,dx)
+x = np.arange(1-10e-12,1+10e-12,dx)
 
 #Defines the limits for the space (J) and time (T) range being considered.
 J = len(x)
-T = 5000
+T = 1000
 
 #Define P0, H0, omega, and alpha.
 P0 = 28*60*60*24
@@ -71,15 +80,15 @@ alpha = dt/(2*dx**2)
 
 #Defines the drift coefficient matrix (D1) using previously defined variables.
 D1Values = np.array([(3/160)*P**2*(H0**2)*(P0**2)*(-79*omega + 288*omega - 27*omega) for P in x])
-D1_0 = 0*diags([D1Values[:J-1], -D1Values[1:J]], [-1,1], shape=(J,J))
-D1 = diags([D1Values[:J-1], -D1Values[1:J]], [-1,1], shape=(J,J))
+D1_0 = np.zeros((J,J))
+D1 = diags([D1Values[:J-1], -D1Values[1:J]], [-1,1], shape=(J,J)).toarray()
 
 #Defines the diffusion coefficient matrix (D2) using previously defined variables.
 D2_c = np.diag([(27/20)*(H0**2)*(P0**2)*omega for P in x])       #For constant D2
 D2 = np.diag([(27/20)*P**3*(H0**2)*(P0**2)*omega for P in x])  #For variable D2
 
 #Defines the initial W vector as a narrow Gaussian 
-variance = (5*dx)**2
+variance = (20*dx)**2
 W = exactSol(variance)
 
 #Defines the M and I matrices as outlined in the progress report.
@@ -107,10 +116,10 @@ Wdata_1 = [W]
 Wdata_2 = [W]
 Wdata_3 = [W]
 
-exactMoment = [findMoment(4, W)/(findMoment(2,W)**(2))]
-momentData_1 = [findMoment(4, W)/(findMoment(2,W)**(2))]
-momentData_2 = [findMoment(4, W)/(findMoment(2,W)**(2))]
-momentData_3 = [findMoment(4, W)/(findMoment(2,W)**(2))]
+exactMoment = [findMoment(3, W)/findMoment(2, W)**(3/2)]
+momentData_1 = [findMoment(3, W)/findMoment(2, W)**(3/2)]
+momentData_2 = [findMoment(3, W)/findMoment(2, W)**(3/2)]
+momentData_3 = [findMoment(3, W)/findMoment(2, W)**(3/2)]
 
 #Run simulation.
 for t in range(0,T-1):
@@ -119,26 +128,31 @@ for t in range(0,T-1):
     """
     variance += 2*(27/20)*(H0**2)*(P0**2)*omega*dt
     exactData.append(exactSol(variance))
-    exactMoment.append(findMoment(4, exactData[t+1])/findMoment(2,exactData[t+1])**(2))
+    exactMoment.append(findMoment(3, exactData[t+1])/findMoment(2, exactData[t+1])**(3/2))
 
-    Wdata_1.append(np.matmul(updateMatrix_1, Wdata_1[t]).tolist()[0])     #Update the distribution at each timestep.
-    momentData_1.append(findMoment(4, Wdata_1[t+1])/findMoment(2,Wdata_1[t+1])**(2))
+    Wdata_1.append(np.matmul(updateMatrix_1, Wdata_1[t]))
+    momentData_1.append(findMoment(3, Wdata_1[t+1])/findMoment(2, Wdata_1[t+1])**(3/2))
 
-    Wdata_2.append(np.matmul(updateMatrix_2, Wdata_2[t]).tolist()[0])
-    momentData_2.append(findMoment(4, Wdata_2[t+1])/findMoment(2,Wdata_2[t+1])**(2))
+    Wdata_2.append(np.matmul(updateMatrix_2, Wdata_2[t]))
+    momentData_2.append(findMoment(3, Wdata_2[t+1])/findMoment(2, Wdata_2[t+1])**(3/2))
 
-    Wdata_3.append(np.matmul(updateMatrix_3, Wdata_3[t]).tolist()[0])
-    momentData_3.append(findMoment(4, Wdata_3[t+1])/findMoment(2,Wdata_3[t+1])**(2))
+    Wdata_3.append(np.matmul(updateMatrix_3, Wdata_3[t]))
+    momentData_3.append(findMoment(3, Wdata_3[t+1])/findMoment(2, Wdata_3[t+1])**(3/2))
 
-# evolutionAnimation()
-plt.title('Standardised Fourth Central Moment Against Time')
-plt.xlabel('Log(Time) / Log(Orbits, $P_0$)')
-plt.ylabel('Log($\mu_4 / \sigma^2$)')
+# evolutionAnimation(exactData)
+plt.title('Skewness Against Time')
+plt.ylabel('$\mu_3 / \sigma^3$')
+plt.xlabel('Time / $P_0$')
 
-#plt.plot(np.linspace(0,dt*T,T),np.abs(exactMoment), label='Exact')
-plt.plot(np.linspace(0,dt*T,T),np.abs(momentData_1), label='$D^{(1)} = 0, D^{(2)} =$ Constant')
-plt.plot(np.linspace(0,dt*T,T),np.abs(momentData_2), label='$D^{(1)} = 0, D^{(2)} =$ Non-Constant')
-plt.plot(np.linspace(0,dt*T,T),np.abs(momentData_3), label='$D^{(1)} = $Non-Zero, $D^{(2)} =$ Non-Constant')
+# plt.plot(np.log10(np.linspace(0,dt*T,T)),np.log10(exactMoment), label='Exact')
+# plt.plot(np.log10(np.linspace(0,dt*T,T)),np.log10(momentData_1), label='$D^{(1)} = 0, D^{(2)} =$ Constant')
+# plt.plot(np.log10(np.linspace(0,dt*T,T)),np.log10(momentData_2), label='$D^{(1)} = 0, D^{(2)} =$ Non-Constant')
+# plt.plot(np.log10(np.linspace(0,dt*T,T)),np.log10(momentData_3), label='$D^{(1)} = $Non-Zero, $D^{(2)} =$ Non-Constant')
+
+plt.plot(np.linspace(0,dt*T,T),exactMoment, label='Exact')
+plt.plot(np.linspace(0,dt*T,T),momentData_1, label='$D^{(1)} = 0, D^{(2)} =$ Constant')
+plt.plot(np.linspace(0,dt*T,T),momentData_2, label='$D^{(1)} = 0, D^{(2)} =$ Non-Constant')
+plt.plot(np.linspace(0,dt*T,T),momentData_3, label='$D^{(1)} = $Non-Zero, $D^{(2)} =$ Non-Constant')
 
 plt.legend()
 plt.show()